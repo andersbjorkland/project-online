09c547eea5cf4527108f60323bceef3c6f3c0a45
Anders Björkland <git@andersbjorkland.online>
2020-12-04 10:59:14 +0100

    Check if server is local or production to adjust including of vendors.

--- public/index.php
+++ public/index.php
@@ -5,9 +5,22 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+$vendors = '';
+if (strpos($_SERVER['SERVER_NAME'], 'localhost') !== false
+    || strpos($_SERVER['SERVER_NAME'], '127.0.0.1') !== false) {
+	$vendors =  dirname(__DIR__).'/vendor/autoload.php';
+	echo "DEV\n";
+} else {
+	$vendors =  dirname(__DIR__).'/vendor/autoload.php';
+	echo "PROD!\n";
+}
+echo $_SERVER['SERVER_NAME'];
+
+require $vendors;
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+dd($vendors);
+
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
