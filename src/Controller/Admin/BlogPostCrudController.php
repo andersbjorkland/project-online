5908150b75d387a8f2d71c5aa8a00bbf43c7ca2f
Anders Björkland <git@andersbjorkland.online>
2020-12-06 12:19:33 +0100

    Added slug updating through crudcontroller.

--- src/Controller/Admin/BlogPostCrudController.php
+++ src/Controller/Admin/BlogPostCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\BlogPost;
 use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -36,11 +37,38 @@ class BlogPostCrudController extends AbstractCrudController
 	    ];
     }
 
-    public function createEntity( string $entityFqcn ) {
+    public function persistEntity( EntityManagerInterface $entityManager, $entityInstance ): void {
+    	$entityInstance->setSlug(
+    		$entityInstance->getPublishAt()->format('Y/m/d/') .
+		    rawurlencode($entityInstance->getTitle())
+        );
+
+    	if (!$entityInstance->getPermanentSlug()) {
+    		$entityInstance->setPermanentSlug(rawurlencode(uniqid()));
+	    }
+
+	    parent::persistEntity( $entityManager, $entityInstance );
+    }
+
+    public function updateEntity( EntityManagerInterface $entityManager, $entityInstance ): void {
+	    $entityInstance->setSlug(
+		    $entityInstance->getPublishAt()->format('Y/m/d/') .
+		    rawurlencode($entityInstance->getTitle())
+	    );
+
+	    if (!$entityInstance->getPermanentSlug()) {
+		    $entityInstance->setPermanentSlug(rawurlencode(uniqid()));
+	    }
+
+	    parent::updateEntity( $entityManager, $entityInstance );
+    }
+
+	public function createEntity( string $entityFqcn ) {
 	    $blogPost = new BlogPost();
 	    $blogPost->setUser($this->getUser() );
 	    $blogPost->setCreatedAt(new DateTime());
 	    $blogPost->setModifiedAt(new DateTime());
+	    $blogPost->setPermanentSlug(uniqid());
 	    return $blogPost;
     }
 }
